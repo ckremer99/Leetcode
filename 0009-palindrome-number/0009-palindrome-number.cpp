c7e5cbd1e143a6736626c16955f8b1d41cd5fa7d
Cody <ckremer99@outlook.com>
2024-09-22 13:24:49 -0700

    Time: 11 ms (41.59%), Space: 8.5 MB (37.2%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string> 
+
+using namespace std; 
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false; 
+        int reversedHalf = 0; 
+        while(reversedHalf < x) {
+            reversedHalf = reversedHalf * 10 + x % 10; 
+            x /= 10; 
+        }
+
+        return (x == reversedHalf || x == reversedHalf / 10); 
+    }
+};
